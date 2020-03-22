ce9b163ff6fa71c7f484362485b71bfd3215092b
Lucas Schneider <lucastrschneider@gmail.com>
2020-03-22 18:52:09 -0300

    :recycle: Refactor example to make it simple

--- src/main.c
+++ src/main.c
@@ -11,13 +11,9 @@
  * Private Constants
  *****************************************/
 
-#define NUMBER_OF_TOGGLES 6
+#define LED_ON_TIMES 4
 
-#define FIRST_TOGGLE_TIME 100
-#define TOGGLE_TIME_STEP 200
-
-/** @brief Calculates the timer period using the sum of the terms from an arithmetic progression */
-#define TIM_PERIOD (2 * FIRST_TOGGLE_TIME + TOGGLE_TIME_STEP * NUMBER_OF_TOGGLES) * (NUMBER_OF_TOGGLES + 1) / 2
+#define TOGGLE_TIME_STEP 150
 
 /*****************************************
  * Private Variables
@@ -31,7 +27,7 @@ uint8_t ap_index = 1;
 /**
  * @brief Value to compare with timer counter.
  */
-uint16_t tim_compare = FIRST_TOGGLE_TIME;
+uint16_t tim_compare = TOGGLE_TIME_STEP;
 
 /*****************************************
  * Main Function
@@ -43,27 +39,23 @@ int main(void) {
     MX_TIM6_Init();
     HAL_TIM_Base_Start_IT(&htim6);
 
-    __HAL_TIM_SET_AUTORELOAD(&htim6, TIM_PERIOD);
-
     HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
 
     for (;;) {
-        uint16_t tim_counter = __HAL_TIM_GET_COUNTER(&htim6);
+        if (__HAL_TIM_GET_COUNTER(&htim6) > tim_compare) {
+            __HAL_TIM_SET_COUNTER(&htim6, 0);
 
-        if (tim_counter > tim_compare) {
-            HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-            tim_compare += (FIRST_TOGGLE_TIME + TOGGLE_TIME_STEP * ap_index); /**< Sums the next term of the AP */
+            if (ap_index > (LED_ON_TIMES * 2)) {
+                ap_index = 0;
+            } else {
+                HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+            }
+
             ap_index++;
+
+            tim_compare = TOGGLE_TIME_STEP * ap_index;
         }
 
         /* Aqui o resto do código pode ser executado */
-    }
-}
-
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef* htim) {
-    if (htim == &htim6) {
-        ap_index = 1;
-        tim_compare = FIRST_TOGGLE_TIME;
-        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
     }
 }
